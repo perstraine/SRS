85d85abf0c587e0002cd7d90a65b016a78a450b4
perstraine <41206120+perstraine@users.noreply.github.com>
2023-02-16 12:15:56 +1300

    Header and Homepage made responsive

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -4,13 +4,16 @@ import { useNavigate ,useLocation} from 'react-router-dom';
 import { useState } from 'react';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faAngleDown } from "@fortawesome/free-solid-svg-icons";
+// import useWindowDimensions from '../../useWindowDimensions';
 
 export default function Header() {
+  // const { width } = useWindowDimensions();
+  // const screenWidth = width;
     const navigate = useNavigate();
     const location = useLocation();
   const [active, setActive] = useState(location.pathname)
   const [drop, setDrop] = useState(false)
-  const [isOpen, setIsOpen] = useState("false");
+  const [isopen, setIsOpen] = useState("false");
     return (
       <div id={styles.headerContainer}>
         <div id={styles.logomotto}>
@@ -27,7 +30,7 @@ export default function Header() {
           </div>
           <div
             id={styles.hamburger}
-            isOpen={isOpen}
+            isopen={isopen}
             onClick={() => {
               setIsOpen("true");
             }}
@@ -35,7 +38,7 @@ export default function Header() {
             {"\u2630"}
           </div>
 
-          <div id={styles.navbarModal} isOpen={isOpen}>
+          <div id={styles.navbarModal} isopen={isopen}>
             <div
               id={drop ? styles.activeDrop : styles.inactiveDrop}
               onMouseEnter={() => {
@@ -86,10 +89,10 @@ export default function Header() {
                 Specialist
               </div>
             </div>
-            <div id={styles.navbar} isOpen={isOpen}>
+            <div id={styles.navbar} isopen={isopen}>
               <div
                 id={styles.close}
-                isOpen={isOpen}
+                isopen={isopen}
                 onClick={() => {
                   setIsOpen("false");
                 }}
@@ -115,8 +118,61 @@ export default function Header() {
                 onMouseLeave={() => {
                   setDrop(false);
                 }}
+                onClick={() => {
+                  setDrop(!drop);
+                }}
               >
                 What We Do <FontAwesomeIcon icon={faAngleDown} />
+                <div
+                  id={drop ? styles.inlineActiveDrop : styles.inactiveDrop}
+                  onMouseEnter={() => {
+                    setDrop(true);
+                  }}
+                  onMouseLeave={() => {
+                    setDrop(false);
+                  }}
+                >
+                  <div
+                    className={styles.dropItem}
+                    onClick={() => {
+                      setActive("/suspendedceiling");
+                      navigate("/suspendedceiling");
+                      setIsOpen("false");
+                    }}
+                  >
+                    Suspended Ceiling
+                  </div>
+                  <div
+                    className={styles.dropItem}
+                    onClick={() => {
+                      setActive("/wallandparition");
+                      navigate("/wallandpartition");
+                      setIsOpen("false");
+                    }}
+                  >
+                    Wall & Partition
+                  </div>
+                  <div
+                    className={styles.dropItem}
+                    onClick={() => {
+                      setActive("/services");
+                      navigate("/services");
+                      setIsOpen("false");
+                    }}
+                  >
+                    Services
+                  </div>
+                  <div
+                    className={styles.dropItem}
+                    onClick={() => {
+                      setActive("/specialist");
+                      navigate("/specialist");
+                      setIsOpen("false");
+                    }}
+                  >
+                    Specialist
+                  </div>
+                </div>
               </div>
               <div
                 id={active === "/mission" ? styles.active : null}
